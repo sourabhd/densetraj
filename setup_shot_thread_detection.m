15c95b01c43f79da003c8cf94149cad4a77b30ea
Sourabh Daptardar <sdaptardar@cs.stonybrook.edu>
2014-10-13 19:17:34 -0400

    Setup environment for shot and thread detection.

---
+++ setup_shot_thread_detection.m
@@ -0,0 +1,4 @@
+addpath(genpath('/nfs/bigeye/sdaptardar/actreg/MyGradFuncs'))
+run('/nfs/bigeye/sdaptardar/actreg/voc-release5/startup.m')
+addpath('/nfs/bigeye/sdaptardar/actreg/voc-release5/bin')
+run('/nfs/bigeye/sdaptardar/installs/vlfeat/toolbox/vl_setup.m')
