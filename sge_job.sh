16525a1c142cc54c3253c955c8cc605d8204d39b
Sourabh Daptardar <sdaptardar@cs.stonybrook.edu>
2014-11-21 10:38:36 -0500

    Invoke matlab in different way in job script

--- sge_job.sh
+++ sge_job.sh
@@ -6,7 +6,7 @@
 #$ -e /nfs/bigeye/sdaptardar/logs/log.$JOB_ID.err
 #$-M sdaptardar@cs.stonybrook.edu
 #$-m ea
-#$-pe default 4
+#$-pe default 6
 
 export LD_LIBRARY_PATH=/opt/matlab_r2014a/bin/glnxa64:/usr/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}
 export DISPLAY=localhost:10.0
@@ -15,5 +15,5 @@ echo "Starting job: $JOB_ID"
 #####matlab -nodesktop -nosplash < /nfs/bigeye/sdaptardar/actreg/densetraj/run_all_gmm.m
 #####matlab -nodesktop -nosplash < /nfs/bigeye/sdaptardar/actreg/densetraj/run_all_fisher_encode.m
 #####matlab -nodesktop -nosplash < /nfs/bigeye/sdaptardar/actreg/densetraj/run_thread_fv_simple.m
-matlab -nodesktop -nosplash < /nfs/bigeye/sdaptardar/actreg/densetraj/run_all_thread_classifier.m
+matlab -nodesktop -nosplash -r "run('/nfs/bigeye/sdaptardar/actreg/densetraj/run_all_thread_classifier.m'); exit;"
 echo "Ending job: $JOB_ID"
